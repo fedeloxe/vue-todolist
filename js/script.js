1129e8cc0c3c595c9945a93020d6ff87a0fe1474
fedeloxe <loxeee@gmail.com>
2022-12-20 17:27:31 +0100

    milestone 1, 2 ,3 , bonus 1 done

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            newTask:'',
+            toDoList:[
+                {
+                    text: 'Chiamare Banca',
+                    done: false,
+                },
+                {
+                    text: 'Prendere appuntamento dal barbiere',
+                    done: false,
+                },
+                {
+                    text: 'Prenotare tavolo per cena',
+                    done: true,
+                },
+                {
+                    text: 'Guardare nuovo ep. chainsawman',
+                    done: false,
+                },
+                {
+                    text: 'Montare luci di natale',
+                    done: true,
+                }
+            ]
+        }
+    },
+    methods:{
+        addTask(){
+            let object = {
+                text: this.newTask,
+                done: false,
+            }
+            this.toDoList.push(object);
+            this.newTask = '';
+        },
+        removeTask(index){
+            this.toDoList.splice(index, 1);
+        },
+        checkTask(index)
+        {
+            let task = this.toDoList[index];
+
+            if(task.done == true){
+                task.done= false;
+            }
+            else{
+                task.done= true;
+            }
+        }
+    },
+}).mount('#app')
